90d61ed0b5b392c9dcbae2880dd429b143241973
vidrasenpai <88034866+vidrasenpai@users.noreply.github.com>
2024-03-27 03:26:10 +0700

    config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7137437536:AAGWGB03f-tP14Tpv4H9muOVQv5Lomj1nwo")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6620140140:AAFg3KowLo_my2wREmf2y9hPoWZ84WQnKdk")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "7375040"))
@@ -20,13 +20,13 @@ APP_ID = int(os.environ.get("APP_ID", "7375040"))
 API_HASH = os.environ.get("API_HASH", "4166e18db5a7880136d41ceb0aa20971")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002026275352"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001964642092"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "1880970848")
 
 # Protect Content
-PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "True"))
+PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
 
 # Heroku Credentials for updater.
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
@@ -36,11 +36,11 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://wpjonory:LjwL9kcf0IqCyp0mVbir-d0aqrcO83oR@rain.db.elephantsql.com/wpjonory")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://beywprij:IMlqqiUr_bELvYb5KbfLxEkHF1SXJIo-@satao.db.elephantsql.com/beywprij")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001168297108"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001698540394"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001828491476"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001924194314"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
 
@@ -68,7 +68,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((844432220, 1250450587, 1750080384, 182990552, 5198570330, 5145083028, 1975330318, 1305389342))
+ADMINS.extend((844432220, 1250450587, 1750080384, 182990552, 5574755137, 6103551145, 6448152026))
 
 
 LOG_FILE_NAME = "logs.txt"
